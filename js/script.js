e74860eb2ebf8e98f9517b94aeb9e7804605a117
26052 <azita.zeyghami@aruplab.com>
2020-11-29 12:29:07 -0700

    updated with radial chart and csv file

--- js/script.js
+++ js/script.js
@@ -2,12 +2,16 @@ load_categories = d3.json('data/categories.json');
 load_spirits = d3.csv('data/spirits.csv');
 load_wine = d3.csv('data/wine.csv');
 load_beer = d3.csv('data/beer.csv');
-let promises = [load_categories, load_spirits, load_wine, load_beer];
+load_month = d3.csv('data/MonthSales.csv');
+let promises = [load_categories, load_spirits, load_wine, load_beer, load_month];
 
 Promise.all(promises).then(function(values) {
-    let [categories, spirits, wine, beer] = values;
+    let [categories, spirits, wine, beer, month] = values;
 
     // Create Categories Visualization
     let categoriesChart = new CategoriesChart(categories, spirits, wine, beer);
     categoriesChart.drawTable(null);
+
+    // Create monthly sales visualization
+    let build = new DataPull(month)
 });
