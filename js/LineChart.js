b9057bd43f85aba091482db05fac5e8f6c3b26a5
zacharyshepelak <42554751+zacharyshepelak@users.noreply.github.com>
2020-11-29 20:54:00 -0700

    Add files via upload

---
+++ js/LineChart.js
@@ -0,0 +1,154 @@
+let svg2 = d3.select("#my_dataviz2")
+    .append("svg")
+    .attr("width", 1800)
+    .attr("height", 1200)
+    .append("g")
+    .attr("transform", "translate(" + 80 + "," + 10 + ")");
+
+class Line_Chart{
+    constructor(data){
+        this.data = data
+    }
+
+    drawLine(){
+        let that = this;
+        
+        let names = []
+        this.data.map(function(d) {names.push(d["Product Name"])});
+        let names_set = [...new Set(names)];
+        let names_set_sorted = names_set.sort(function(a, b){
+            return d3.ascending(a,b);
+        });
+        d3.select("#selectName")
+            .selectAll('myOptions')
+            .data(names_set_sorted)
+            .enter().append("option")
+            .text(function(d) { return d; })
+            .attr("value", function (d, i) {
+                return d;
+            });
+
+
+        d3.select("#selectName")
+            .on("change",function(d){
+                console.log(this.value)
+                that.updateLine(this.value)
+            });
+    }
+
+
+
+    updateLine(selectedProduct) {
+        //console.log(data)
+        //svg.selectAll("circle").remove();
+        svg2.selectAll("*").remove();
+
+        let data_filtered_not_sorted = this.data.filter(function(d){ 
+            if((d["Product Name"]) == selectedProduct){ 
+                return d;
+            }
+        });
+
+        let data_filtered1 = data_filtered_not_sorted.sort(function(a, b){
+            return d3.ascending(new Date(a["Date"]), new Date(b["Date"]));
+        });
+        console.log(data_filtered1)
+
+
+        let tooltip = d3.select("#my_dataviz")
+            .append("div")
+            .style("opacity", 0)
+            .attr("class", "tooltip1")
+            .style("background-color", "white")
+            .style("border-radius", "1px")
+            .style("font-size", "10px")
+            .style("border", "solid")
+            .style("border-width", "2px")
+            .style("border-radius", "5px")
+            .style("padding", "0px")
+            .style("color", "black");
+
+
+        let width = 400;
+        let height = 400;
+    
+        let filtered_scaleTotalSales1 = d3.scaleLinear()
+            .domain([0, d3.max(data_filtered1, d => parseInt(d["Total Sales"]))])
+            .range([height, 0]);
+
+        var xScale = d3.scaleTime()
+            .domain([new Date("2018-04-01"), new Date("2020-09-01")])
+            .range([0, width]);
+        
+        var xAxis = d3.axisBottom(xScale)
+            .tickFormat(d3.timeFormat("%b-%Y"));
+
+        svg2.append("g")
+            .attr("transform", "translate(0," + 400 + ")")
+            .call(xAxis.ticks(20))
+            .attr("id", "xaxis")
+            .selectAll("text")	
+            .style("text-anchor", "end")
+            .attr("dx", "-.8em")
+            .attr("dy", ".15em")
+            .attr("transform", "rotate(-65)");
+        svg2.append("text")
+            .attr("fill", "black")
+            .attr("transform","translate(200, 470)")
+            .text("Date");
+
+        svg2.append("g")
+            .call(d3.axisLeft(filtered_scaleTotalSales1));
+        svg2.append("text")
+            .attr("fill", "black")
+            .attr("transform","translate(-75, 210)")
+            .text("Total Sales");
+
+        svg2.append("path")
+            .datum(data_filtered1)
+            .attr("fill", "none")
+            .attr("stroke", "black")
+            .attr("stroke-width", 1.5)
+            .attr("d", d3.line()
+              .x(d => xScale(new Date(d["Date"])))
+              .y(d => filtered_scaleTotalSales1(parseInt(d["Total Sales"])))
+              )
+      
+
+
+        svg2.append('g')
+            .selectAll("circle")
+            .data(data_filtered1)
+            .join("circle")
+            .attr("class", "bubbles")
+            .attr("cx", d => xScale(new Date(d["Date"])))
+            .attr("cy", d => filtered_scaleTotalSales1(parseInt(d["Total Sales"])))
+            .attr("r", 8)//d => this.scaleBottleSize(parseInt(d["Bottle Size"])))
+            .attr("stroke-width", "1px")
+            .attr("stroke", "black")
+            .attr("class", d => d["Class Code"].substring(0,1))
+            .style("opacity", 1)
+            .on("mouseover", function(event,d) {
+                tooltip.transition()
+                    .duration(1)
+                    .style("opacity", 1);
+                    tooltip.html((d["Total Sales"]))
+                    .style("left", (event.pageX - 20) + "px")
+                    .style("top", (event.pageY - 20) + "px");
+                })
+            .on("mouseout", function(event, d) {
+                tooltip
+                    .transition()
+                    .duration(1)
+                    .style("opacity", 0.0000001);
+            });
+
+
+
+        console.log(xScale(new Date("2018-06-01")))
+
+    }
+
+
+
+}
---
+++ js/bubblechart3.js
@@ -0,0 +1,199 @@
+let svg = d3.select("#my_dataviz")
+    .append("svg")
+    .attr("width", 1800)
+    .attr("height", 1200)
+    .append("g")
+    .attr("transform", "translate(" + 80 + "," + 10 + ")");
+
+class Bubble{
+    constructor(data){
+        this.data = data
+
+        this.Pricemin = d3.min(this.data, d => parseInt(d.Price))
+        this.Pricemax = d3.max(this.data, d => parseInt(d.Price))
+        
+        //this.BottleCountmin = d3.min(this.data, d => parseInt(d["Total Bottle Count"]))
+        //this.BottleCountmax = d3.max(this.data, d => parseInt(d["Total Bottle Count"]))
+        
+        this.CaseCountmin = d3.min(this.data, d => parseInt(d["Case Count (9L)"]))
+        this.CaseCountmax = d3.max(this.data, d => parseInt(d["Case Count (9L)"]))
+        
+        this.TotalSalesmin = d3.min(this.data, d => parseInt(d["Total Sales"]))
+        this.TotalSalesmax = d3.max(this.data, d => parseInt(d["Total Sales"]))
+
+        this.BottleSizemin = d3.min(this.data, d => parseInt(d["Bottle Size"]))
+        this.BottleSizemax = d3.max(this.data, d => parseInt(d["Bottle Size"]))
+
+        let width = 1000;
+        let height = 500;
+
+        this.scalePrice = d3.scaleLinear()
+            .domain([this.Pricemin, this.Pricemax])
+            .range([0, height]);
+    
+        this.scaleBottleCount = d3.scaleLinear()
+            .domain([this.BottleCountmin, this.BottleCountmax])
+            .range([height, 0]);
+    
+        this.scaleCaseCount = d3.scaleLinear()
+            .domain([this.CaseCountmin, this.CaseCountmax])
+            .range([0, width]);
+    
+        this.scaleBottleSize = d3.scaleLinear()
+            .domain([this.BottleSizemin, this.BottleSizemax])
+            .range([1, 15]);
+        
+        this.scaleTotalSales = d3.scaleLinear()
+            .domain([this.TotalSalesmin, this.TotalSalesmax])
+            .range([height, 0]);
+        this.drawBubble;
+        this.updateBubble;
+    }
+
+    drawBubble(){
+        let that = this;
+        
+        let dates = []
+        this.data.map(function(d) {dates.push(d.Date)});
+        let dates_set = [...new Set(dates)];
+        let dates_set_sorted = dates_set.sort(function(a, b){
+            return d3.ascending(new Date(a), new Date(b));
+        });
+        d3.select("#selectDate")
+            .selectAll('myOptions')
+            .data(dates_set_sorted)
+            .enter().append("option")
+            .text(function(d) { return d; })
+            .attr("value", function (d, i) {
+                return d;
+            });
+
+
+        let bottlesizes = []
+        this.data.map(function(d) {bottlesizes.push(d["Bottle Size"])});
+        let bottlesizes_set = [...new Set(bottlesizes)];
+        let bottlesizes_set_sorted = bottlesizes_set.sort(function(a, b){
+            return d3.ascending(parseInt(a), parseInt(b));
+        });
+        d3.select("#selectBottleSize")
+            .selectAll('myOptions')
+            .data(bottlesizes_set_sorted)
+            .enter().append("option")
+            .text(function(d) { return d; })
+            .attr("value", function (d, i) {
+                return d;
+            });
+
+        var values = ["2018-04-01", "50"];
+        d3.selectAll(".filters")
+            .on("change",function(d){ 
+                d3.select(this)
+                    .selectAll("option:checked") 
+                    .each(function() {
+                        if (this.value.length > 8) {
+                            values.splice(0,1,this.value) //date
+                        } else {
+                            values.splice(1,1,this.value); //bottle size
+                        }
+                    });
+                console.log(values)
+                //console.log(values[0])
+                that.updateBubble(values[1], values[0])
+            })   
+
+
+    };
+
+
+    
+
+    updateBubble(selectedBottleSize, selectedDate) {
+            //console.log(data)
+            //svg.selectAll("circle").remove();
+            svg.selectAll("*").remove();
+
+            let data_filtered = this.data.filter(function(d){ 
+                if( parseInt(d["Bottle Size"]) == selectedBottleSize && (d["Date"]) == selectedDate){ 
+                return d;
+            }
+            });
+            //console.log(data_filtered)
+
+            let width = 400;
+            let height = 400;
+
+            let filtered_scaleCaseCount = d3.scaleLinear()
+                .domain([d3.min(data_filtered, d => parseInt(d["Case Count (9L)"])), d3.max(data_filtered, d => parseInt(d["Case Count (9L)"]))])
+                .range([0, width]);
+        
+            let filtered_scaleTotalSales = d3.scaleLinear()
+                .domain([d3.min(data_filtered, d => parseInt(d["Total Sales"])), d3.max(data_filtered, d => parseInt(d["Total Sales"]))])
+                .range([height, 0]);
+
+
+            svg.append("g")
+                .attr("transform", "translate(0," + 400 + ")")
+                .call(d3.axisBottom(filtered_scaleCaseCount))
+                .attr("id", "xaxis")
+                .append("text")
+                .attr("fill", "black")
+                .attr("transform","translate(200, 40)")
+                .text("Case Count (9L)");
+    
+            svg.append("g")
+                .call(d3.axisLeft(filtered_scaleTotalSales))
+                .append("text")
+                .attr("fill", "black")
+                .attr("transform","translate(-10, 200)")
+                .text("Total Sales");
+        
+
+
+            let tooltip = d3.select("#my_dataviz")
+                .append("div")
+                .style("opacity", 0)
+                .attr("class", "tooltip")
+                .style("background-color", "white")
+                .style("border-radius", "5px")
+                .style("font-size", "10px")
+                .style("border", "solid")
+                .style("border-width", "2px")
+                .style("border-radius", "5px")
+                .style("padding", "0px")
+                .style("color", "black");
+        
+            svg.append('g')
+                .selectAll("circle")
+                .data(data_filtered)
+                .join("circle")
+                .attr("class", "bubbles")
+                .attr("cx", d => filtered_scaleCaseCount(parseInt(d["Case Count (9L)"])))
+                .attr("cy", d => filtered_scaleTotalSales(parseInt(d["Total Sales"])))
+                .attr("r", 8)//d => this.scaleBottleSize(parseInt(d["Bottle Size"])))
+                .attr("stroke-width", "1px")
+                .attr("stroke", "black")
+                .attr("class", d => d["Class Code"].substring(0,1))
+                .style("opacity", .8)
+                .on("mouseover", function(event,d) {
+                    tooltip.transition()
+                        .duration(1)
+                        .style("opacity", 1);
+                        tooltip.html((d["Product Name"]))
+                        .style("left", (event.pageX - 60) + "px")
+                        .style("top", (event.pageY - 80) + "px");
+                    })
+                .on("mouseout", function(event, d) {
+                    tooltip
+                        .transition()
+                        .duration(1)
+                        .style("opacity", 0);
+                });
+    }
+}
+
+
+
+
+
+
+
